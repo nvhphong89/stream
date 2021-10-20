dfd731376379bae6baf7bc821badb38de535cf8e
nvhphong89 <nvh.phong.89@gmail.com>
2021-10-20 10:39:14 -0400

    added stream show page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "../history";
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
@@ -12,13 +12,15 @@ const App = ()=>{
     return (
         <div className="ui container">
             <Router history={history}>
-                <Header/>
                 <div>
+                <Header/>
+                <Switch>
                     <Route path="/" exact component={StreamList}></Route>
-                    <Route path="/streams/show" exact component={StreamShow}></Route>
                     <Route path="/streams/new" exact component={StreamCreate}></Route>
+                    <Route path="/streams/:id" exact component={StreamShow}></Route>
                     <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
                     <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
+                </Switch>
                 </div>
             </Router>
         </div>
